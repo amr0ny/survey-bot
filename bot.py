b1db901e0fb34eb1fae09f585f89e43de1753b9a
Alex <38862440+amr0ny@users.noreply.github.com>
2023-12-06 00:50:33 +0300

    Refactoring was deepened, txtbtn handler was introduced

--- bot.py
+++ bot.py
@@ -13,13 +13,13 @@ class Bot(TeleBot):
 		self.data_template = [data.get(i) for i in data]
 
 		self.data = {}
-		self.user_ans =  {}
+		self.users_ans =  {}
 		self.__user_msg_id = {}
 		self.csv_table = CSVTable(self.file_path, [i['msg'] for i in self.data_template])
 		
 	def init_user(self, user_id):
 		self.data[user_id] = self.data_template
-		self.user_ans[user_id] = {self.data_template[index]['msg']: '' for index, _ in enumerate(self.data_template)}
+		self.users_ans[user_id] = {self.data_template[index]['msg']: '' for index, _ in enumerate(self.data_template)}
 		self.__user_msg_id[user_id] = 0
 
 	def set_msg_id(self, user_id, msg_id):
@@ -31,12 +31,14 @@ class Bot(TeleBot):
 	def build_msg(self, message):
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		ans_type = self.data[user_id][msg_id]['ans_type']
-		msg_text = self.data[user_id][msg_id]['msg']
+		user_data = self.data[user_id]
+
+		ans_type = user_data[msg_id]['ans_type']
+		msg_text = user_data[msg_id]['msg']
 		markup = None
 		if ans_type == 'btn':
 			markup = types.ReplyKeyboardMarkup(row_width=4, one_time_keyboard=True, resize_keyboard=True, selective=False)	
-			[markup.add(btn) for btn in self.data[user_id][msg_id]['buttons']]
+			[markup.add(btn) for btn in user_data[msg_id]['buttons']]
 		return msg_text, markup
 
 	def send_msg(self, message):
@@ -45,65 +47,109 @@ class Bot(TeleBot):
 		return msg
 	
 	def handle_txt_message(self, message):
+		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		self.user_ans[user_id][self.data[user_id][msg_id-1]['msg']] = message.text
-		if 'next_msg_id' in self.data[user_id][msg_id-1]:
-			self.set_msg_id(user_id, self.data[user_id][msg_id-1]['next_msg_id'])
+		user_data = self.data[user_id]
+		user_ans = self.users_ans[user_id]
+	
+
+		user_ans[user_data[msg_id-1]['msg']] = message.text
+		if 'next_msg_id' in user_data[msg_id-1]:
+			self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id'])
 			msg = self.send_msg(message)
-			self.set_msg_id(user_id, self.data[user_id][msg_id-1]['next_msg_id']+1)
+			self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id']+1)
 		else:
 			msg = self.send_msg(message)
 			self.set_msg_id(user_id, msg_id+1)
+		return msg
 
 	def handle_btn_message(self, message):
+		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		buttons = self.data[user_id][msg_id-1]['buttons']
+		user_data = self.data[user_id]
+		user_ans = self.users_ans[user_id]
+
+		buttons = user_data[msg_id-1]['buttons']
 		if message.text in buttons:
-			self.user_ans[user_id][self.data[user_id][msg_id-1]['msg']] = message.text
-			self.set_msg_id(user_id, self.data[user_id][msg_id-1]['buttons'][message.text]['msg_id'])
+			user_ans[user_data[msg_id-1]['msg']] = message.text
+			self.set_msg_id(user_id, user_data[msg_id-1]['buttons'][message.text]['msg_id'])
 			msg = self.send_msg(message)
-			self.set_msg_id(user_id, self.data[user_id][msg_id-1]['buttons'][message.text]['msg_id']+1)
+			self.set_msg_id(user_id, user_data[msg_id-1]['buttons'][message.text]['msg_id']+1)
 		else:
 			msg = super().send_message(message.chat.id, 'You need to choose a button.')
+		return msg
 
-	def handle_pic_message(self, message):
+	def handle_txtbtn_message(self, message):
+		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		photo_id = message.photo[-1].file_id
+		user_data = self.data[user_id]
+		user_ans = self.users_ans[user_id]
+
+		buttons = user_data[msg_id-1]['buttons']
+		if message.text in buttons:
+			user_ans[user_data[msg_id-1]['msg']] = message.text
+			self.set_msg_id(user_id, user_data[msg_id-1]['buttons'][message.text]['msg_id'])
+			msg = a
+			self.set_msg_id(user_id, user_data[msg_id-1]['buttons'][message.text]['msg_id']+1)
+		else:
+			user_ans[user_data[msg_id-1]['msg']] = message.text
+			if 'next_msg_id' in user_data[msg_id-1]:
+				self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id'])
+				msg = self.send_msg(message)
+				self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id']+1)
+			else:
+				msg = self.send_msg(message)
+				self.set_msg_id(user_id, msg_id+1)
+		return msg
+
+	def handle_pic_message(self, message):
+		msg = message
+		user_id = message.from_user.id
+		msg_id = self.__user_msg_id[user_id]
+		user_data = self.data[user_id]
+		user_ans = self.users_ans[user_id]
+
 		if message.content_type == 'photo':
+			photo_id = message.photo[-1].file_id
 			print(super().get_file_url(photo_id))
 			response = requests.get(super().get_file_url(photo_id))
 			path_to_photo = f'./photos/f{str(uuid.uuid4())}-{message.from_user.username}.jpg'
 			with open(path_to_photo, 'wb') as file:
 				file.write(response.content)
-			self.user_ans[user_id][self.data[user_id][msg_id-1]['msg']] = path_to_photo
-			if 'next_msg_id' in self.data[user_id][msg_id-1]:
-				self.set_msg_id(user_id, self.data[user_id][msg_id-1]['next_msg_id'])
+			user_ans[user_data[msg_id-1]['msg']] = path_to_photo
+			if 'next_msg_id' in user_data[msg_id-1]:
+				self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id'])
 				msg = self.send_msg(message)
-				self.set_msg_id(user_id,self.data[user_id][msg_id-1]['next_msg_id']+1)
+				self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id']+1)
 			else:
 				msg = self.send_msg(message)
 				self.set_msg_id(user_id, msg_id+1)
 		else:
 			msg = super().send_message(message.chat.id, 'You need to send a photo.')
 
+		return msg
+
 	def handle_message(self, message):
 		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		ans_type = self.data[user_id][msg_id-1]['ans_type']
+		user_data = self.data[user_id]
+		user_ans = self.users_ans[user_id]
+		ans_type = user_data[msg_id-1]['ans_type']
+		print(f'{user_data[msg_id-1]["msg"]} - {message.text}, msg_id: {self.get_msg_id(user_id)}')
 		
-		print(f'{self.data[user_id][msg_id-1]["msg"]} - {message.text}, msg_id: {self.get_msg_id(user_id)}')
-		
-		is_last = 'is_last' in self.data[user_id][msg_id-1] and self.data[user_id][msg_id-1]['is_last'] == 'Yes'
+		is_last = 'is_last' in user_data[msg_id-1] and user_data[msg_id-1]['is_last'] == 'Yes'
 		match ans_type:
 			case 'txt':
-				self.handle_txt_message(message)
+				msg = self.handle_txt_message(message)
 			case 'btn':
-				self.handle_btn_message(message)
+				msg = self.handle_btn_message(message)
 			case 'pic':
-				self.handle_pic_message(message)
-		self.csv_table.append_user(self.user_ans[user_id]) and print('success') if is_last else None
+				msg = self.handle_pic_message(message)
+			case 'txtbtn':
+				msg = self.handle_txtbtn_message(message)
+		self.csv_table.append_user(user_ans) and print('success') if is_last else None
 		return super().register_next_step_handler(msg, self.handle_message)
