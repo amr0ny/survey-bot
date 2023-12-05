c29a6d1504cf1c5f92e5ad3897c53acbc8e970a0
Alex <38862440+amr0ny@users.noreply.github.com>
2023-12-06 01:53:33 +0300

    refactoring process

--- bot.py
+++ bot.py
@@ -13,15 +13,15 @@ class Bot(TeleBot):
 			self.photo_directory = config['photo_directory']
 		self.token = token
 		super().__init__(self.token)
-		self.data_template = [data.get(i) for i in data]
-		self.data = {}
+		self.scenario_data_template = [data.get(i) for i in data]
+		self.scenario_data = {}
 		self.users_ans =  {}
 		self.__user_msg_id = {}
-		self.csv_table = CSVTable(self.user_data_path, [i['msg'] for i in self.data_template])
+		self.csv_table = CSVTable(self.user_data_path, [i['msg'] for i in self.scenario_data_template])
 		
 	def init_user(self, user_id):
-		self.data[user_id] = self.data_template
-		self.users_ans[user_id] = {self.data_template[index]['msg']: '' for index, _ in enumerate(self.data_template)}
+		self.scenario_data[user_id] = self.scenario_data_template
+		self.users_ans[user_id] = {self.scenario_data_template[index]['msg']: '' for index, _ in enumerate(self.scenario_data_template)}
 		self.__user_msg_id[user_id] = 0
 
 	def set_msg_id(self, user_id, msg_id):
@@ -33,14 +33,14 @@ class Bot(TeleBot):
 	def build_msg(self, message):
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		user_data = self.data[user_id]
+		user_scenario_data = self.scenario_data[user_id]
 
-		ans_type = user_data[msg_id]['ans_type']
-		msg_text = user_data[msg_id]['msg']
+		ans_type = user_scenario_data[msg_id]['ans_type']
+		msg_text = user_scenario_data[msg_id]['msg']
 		markup = None
 		if ans_type == 'btn':
 			markup = types.ReplyKeyboardMarkup(row_width=4, one_time_keyboard=True, resize_keyboard=True, selective=False)	
-			[markup.add(btn) for btn in user_data[msg_id]['buttons']]
+			[markup.add(btn) for btn in user_scenario_data[msg_id]['buttons']]
 		return msg_text, markup
 
 	def send_msg(self, message):
@@ -52,14 +52,14 @@ class Bot(TeleBot):
 		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		user_data = self.data[user_id]
+		user_scenario_data = self.scenario_data[user_id]
 		user_ans = self.users_ans[user_id]
 
-		user_ans[user_data[msg_id-1]['msg']] = message.text
-		if 'next_msg_id' in user_data[msg_id-1]:
-			self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id'])
+		user_ans[user_scenario_data[msg_id-1]['msg']] = message.text
+		if 'next_msg_id' in user_scenario_data[msg_id-1]:
+			self.set_msg_id(user_id, user_scenario_data[msg_id-1]['next_msg_id'])
 			msg = self.send_msg(message)
-			self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id']+1)
+			self.set_msg_id(user_id, user_scenario_data[msg_id-1]['next_msg_id']+1)
 		else:
 			msg = self.send_msg(message)
 			self.set_msg_id(user_id, msg_id+1)
@@ -69,15 +69,15 @@ class Bot(TeleBot):
 		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		user_data = self.data[user_id]
+		user_scenario_data = self.scenario_data[user_id]
 		user_ans = self.users_ans[user_id]
 
-		buttons = user_data[msg_id-1]['buttons']
+		buttons = user_scenario_data[msg_id-1]['buttons']
 		if message.text in buttons:
-			user_ans[user_data[msg_id-1]['msg']] = message.text
-			self.set_msg_id(user_id, user_data[msg_id-1]['buttons'][message.text]['msg_id'])
+			user_ans[user_scenario_data[msg_id-1]['msg']] = message.text
+			self.set_msg_id(user_id, user_scenario_data[msg_id-1]['buttons'][message.text]['msg_id'])
 			msg = self.send_msg(message)
-			self.set_msg_id(user_id, user_data[msg_id-1]['buttons'][message.text]['msg_id']+1)
+			self.set_msg_id(user_id, user_scenario_data[msg_id-1]['buttons'][message.text]['msg_id']+1)
 		else:
 			msg = super().send_message(message.chat.id, 'You need to choose a button.')
 		return msg
@@ -86,21 +86,21 @@ class Bot(TeleBot):
 		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		user_data = self.data[user_id]
+		user_scenario_data = self.scenario_data[user_id]
 		user_ans = self.users_ans[user_id]
 
-		buttons = user_data[msg_id-1]['buttons']
+		buttons = user_scenario_data[msg_id-1]['buttons']
 		if message.text in buttons:
-			user_ans[user_data[msg_id-1]['msg']] = message.text
-			self.set_msg_id(user_id, user_data[msg_id-1]['buttons'][message.text]['msg_id'])
+			user_ans[user_scenario_data[msg_id-1]['msg']] = message.text
+			self.set_msg_id(user_id, user_scenario_data[msg_id-1]['buttons'][message.text]['msg_id'])
 			msg = self.send_msg(message)
-			self.set_msg_id(user_id, user_data[msg_id-1]['buttons'][message.text]['msg_id']+1)
+			self.set_msg_id(user_id, user_scenario_data[msg_id-1]['buttons'][message.text]['msg_id']+1)
 		else:
-			user_ans[user_data[msg_id-1]['msg']] = message.text
-			if 'next_msg_id' in user_data[msg_id-1]:
-				self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id'])
+			user_ans[user_scenario_data[msg_id-1]['msg']] = message.text
+			if 'next_msg_id' in user_scenario_data[msg_id-1]:
+				self.set_msg_id(user_id, user_scenario_data[msg_id-1]['next_msg_id'])
 				msg = self.send_msg(message)
-				self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id']+1)
+				self.set_msg_id(user_id, user_scenario_data[msg_id-1]['next_msg_id']+1)
 			else:
 				msg = self.send_msg(message)
 				self.set_msg_id(user_id, msg_id+1)
@@ -110,7 +110,7 @@ class Bot(TeleBot):
 		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		user_data = self.data[user_id]
+		user_scenario_data = self.scenario_data[user_id]
 		user_ans = self.users_ans[user_id]
 
 		if message.content_type == 'photo':
@@ -120,11 +120,11 @@ class Bot(TeleBot):
 			path_to_photo = f'{self.photo_directory}/f{str(uuid.uuid4())}-{message.from_user.username}.jpg'
 			with open(path_to_photo, 'wb') as file:
 				file.write(response.content)
-			user_ans[user_data[msg_id-1]['msg']] = path_to_photo
-			if 'next_msg_id' in user_data[msg_id-1]:
-				self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id'])
+			user_ans[user_scenario_data[msg_id-1]['msg']] = path_to_photo
+			if 'next_msg_id' in user_scenario_data[msg_id-1]:
+				self.set_msg_id(user_id, user_scenario_data[msg_id-1]['next_msg_id'])
 				msg = self.send_msg(message)
-				self.set_msg_id(user_id, user_data[msg_id-1]['next_msg_id']+1)
+				self.set_msg_id(user_id, user_scenario_data[msg_id-1]['next_msg_id']+1)
 			else:
 				msg = self.send_msg(message)
 				self.set_msg_id(user_id, msg_id+1)
@@ -137,12 +137,12 @@ class Bot(TeleBot):
 		msg = message
 		user_id = message.from_user.id
 		msg_id = self.__user_msg_id[user_id]
-		user_data = self.data[user_id]
+		user_scenario_data = self.scenario_data[user_id]
 		user_ans = self.users_ans[user_id]
-		ans_type = user_data[msg_id-1]['ans_type']
-		print(f'{user_data[msg_id-1]["msg"]} - {message.text}, msg_id: {self.get_msg_id(user_id)}')
+		ans_type = user_scenario_data[msg_id-1]['ans_type']
+		print(f'{user_scenario_data[msg_id-1]["msg"]} - {message.text}, msg_id: {self.get_msg_id(user_id)}')
 		
-		is_last = 'is_last' in user_data[msg_id-1] and user_data[msg_id-1]['is_last'] == 'Yes'
+		is_last = 'is_last' in user_scenario_data[msg_id-1] and user_scenario_data[msg_id-1]['is_last'] == 'Yes'
 		match ans_type:
 			case 'txt':
 				msg = self.handle_txt_message(message)
